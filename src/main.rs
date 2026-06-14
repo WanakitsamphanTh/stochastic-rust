99faa370ba1b48fa2f4e1270f55c62616e210a40
WanakitsamphanTh <s2101125@sendai-nct.jp>
2026-06-14 14:47:53 +0900

    Add trait MarkovChain; Add CTMC

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,86 @@
-use ndarray::{arr1,arr2,s};
-
-use crate::markov::{DTMC, MarkovChain};
+use crate::markov::{DTMC, CTMC, MarkovChain};
 mod markov;
 
-fn main() {
-    let p = arr2(&[
-        [0.0,0.5,0.5,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.5,0.5,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.5,0.5,0.0,0.0,0.0,0.0,0.0,0.0],
-        [0.0,0.5,0.0,0.0,0.0,0.0,0.0,0.5,0.0,0.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.5,0.5,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.5,0.5,0.0],
-        [0.0,0.0,0.5,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.5],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0],
-        [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0]
-    ]);
+fn knuth_die() {
+    let transition = |curr: usize, next: usize| -> f32  {
+        match curr {
+            0 => match next {
+                1 => 0.5,
+                2 => 0.5,
+                _ => 0.0
+            },
+            1 => match next {
+                3 => 0.5,
+                4 => 0.5,
+                _ => 0.0
+            },
+            2 => match next {
+                5 => 0.5,
+                6 => 0.5,
+                _ => 0.0
+            },
+            3 => match next {
+                1 => 0.5,
+                7 => 0.5,
+                _ => 0.0
+            }
+            4 => match next {
+                8 => 0.5,
+                9 => 0.5,
+                _ => 0.0
+            }
+            5 => match next {
+                2 => 0.5,
+                10 => 0.5,
+                _ => 0.0
+            }
+            6 => match next {
+                11 => 0.5,
+                12 => 0.5,
+                _ => 0.0
+            }
+            7..=12 => if next == curr {1.0} else {0.0},
+            _ => 0.0
+        }
+    };
 
-    let mut dtmc: DTMC = DTMC::new(p.clone(),0, 13);
+    let mut dtmc: DTMC = DTMC::new(0, 1.0,13, transition);
 
-    for i in 1..=30 {
-        dtmc.next_step();
-        let states = dtmc.state();
-        print!("i = {0}; {1}\n", i, states.slice(s![7..=12]));
-    }
-    dtmc.reset();
-    dtmc.simulate(10);
+    print!("Knuth die algorithm: \n");
+    dtmc.simulate(50);
     let states = dtmc.state();
-    print!("after 15 epochs: {0}\n", states);
+    print!("after 50 steps: {0}\n", states);
+}
+
+fn queue_system() {
+    let init = vec![1.0,0.0,0.0,0.0,0.0,0.0,0.0];
+    let mu = 2.0;
+    let lambda = 5.0;
+    let rate_fn = |curr: usize, next: usize| -> f32 {
+        if curr == 0 {
+            if next == 1 {
+                mu
+            } else {
+                0.0
+            }
+        } else {
+            if next == curr + 1 {
+                lambda
+            } else if next == curr - 1 {
+                mu
+            } else {
+                0.0
+            }
+        }
+    };
+    let mut ctmt: CTMC = CTMC::new(init, 7, rate_fn);
+    print!("Queue system: \n");
+    ctmt.simulate(50.0);
+    let states = ctmt.state();
+    print!("after t=50.0: {0}\n", states);
+}
+
+fn main() {
+    knuth_die();
+    queue_system();
 }
--- src/markov.rs
+++ src/markov.rs
@@ -1,53 +1,81 @@
-use ndarray::{self, Array};
+use ndarray::{self, Array, Array2, Axis};
+use ndarray::{ArrayView2};
+use scirs2_linalg::expm;
 
 pub type TransitionMatrix = ndarray::prelude::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::prelude::Dim<[usize; 2]>, f32>;
 pub type States = ndarray::prelude::ArrayBase<ndarray::OwnedRepr<f32>, ndarray::prelude::Dim<[usize; 1]>, f32>;
 pub type State = usize;
 
-pub trait MarkovChain {
-    fn next_step(&mut self);
-    fn simulate(&mut self, times: i32);
-    fn reset(&mut self);
+
+pub trait MarkovChain<T> {
     fn state(&self) -> States;
+    fn matrix(&self) -> TransitionMatrix;
+    fn simulate(&mut self, t: T);
 }
 
 pub struct DTMC {
-    s0: States,
     s: States,
     p: TransitionMatrix,
 }
 
 impl DTMC {
-    pub fn new(p: TransitionMatrix, s0: State, n_state: usize) -> Self {
+    pub fn new(s0: State, init_prob: f32, n_state: usize, transition_fn: impl Fn(usize, usize) -> f32) -> Self {
+        let p: TransitionMatrix = Array2::from_shape_fn((n_state,n_state), |(s_curr, s_next)| transition_fn(s_curr,s_next));
         let mut init =  Array::from_vec(vec![0.0; n_state]);
-        init[s0] = 1.0;
+        init[s0] = init_prob;
         Self {
-            s0: init.clone(),
-            s: init.clone(),
-            p: p.clone()
+            s: init,
+            p: p
         }
     }
-}
-
-impl MarkovChain for DTMC {
     fn next_step(&mut self)  {
         self.s = self.s.dot(&self.p);
     }
-    fn reset(&mut self) {
-        self.s = self.s0.clone();
+}
+
+impl MarkovChain<i32> for DTMC {
+    fn state(&self) -> States {
+        return self.s.clone();
+    }
+    fn matrix(&self) -> TransitionMatrix {
+        return self.p.clone();
     }
     fn simulate(&mut self, times: i32) {
         for _ in 1..=times {
             self.next_step();
         }
     }
-    fn state(&self) -> States {
-        return self.s.clone();
-    }
 }
 
 pub struct CTMC {
-    s0: States,
     s: States,
-    p: TransitionMatrix,
+    rate: TransitionMatrix,
+}
+
+impl CTMC {
+    pub fn new(init_rate: Vec<f32>, n_state: usize, rate_fn: impl Fn(usize, usize) -> f32) -> Self {
+        let mut p: TransitionMatrix = Array2::from_shape_fn((n_state,n_state), |(s_curr, s_next)| rate_fn(s_curr,s_next));
+        let diag = p.sum_axis(Axis(1));
+        for i in 0..n_state {
+            p[[i,i]] = -diag[i];
+        }
+        let init =  Array::from_vec(init_rate);
+        Self {
+            s: init,
+            rate: p,
+        }
+    }
+}
+
+impl MarkovChain<f32> for CTMC {
+    fn state(&self) -> States {
+        return self.s.clone();
+    }
+    fn matrix(&self) -> TransitionMatrix {
+        return self.rate.clone();
+    }
+    fn simulate(&mut self, t: f32){
+        let p = expm(&(&self.rate * t).view(),None).expect("Simulation failed");
+        self.s = self.s.dot(&p);
+    }
 }
